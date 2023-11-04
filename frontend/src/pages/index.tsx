b129ae0a01a02117e36f45e12cffa68e41a4df39
zezerinodev <mattoszz@hotmail.com>
2023-11-04 20:00:36 -0300

    Componente de button construido.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,25 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
+
+//MY IMPORTS
 import styles from '@/styles/Home.module.scss';
+import { Button } from '../components/UI/Button';
+
 
 export default function Home() {
+
+  const [loading, setLoaging] = useState(false);
+
   return (
     <>
       <Head>
         <title>Sistema - dashboard</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
-        <h1>Home</h1>
+
+      <main className={styles.container}>
+        <div className={styles.header}>
+          <Button loading={loading}></Button>
+        </div>
       </main>
     </>
   )
